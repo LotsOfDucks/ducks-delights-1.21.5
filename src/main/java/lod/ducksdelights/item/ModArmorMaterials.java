b1c72ec8d2432a229829df0da00349edf0bc81cd
LotsOfDucks <113879225+LotsOfDucks@users.noreply.github.com>
2025-05-03 16:54:13 -0700

    Haunted Steel Armor Recipe

--- src/main/java/lod/ducksdelights/item/ModArmorMaterials.java
+++ src/main/java/lod/ducksdelights/item/ModArmorMaterials.java
@@ -2,12 +2,14 @@ package lod.ducksdelights.item;
 
 import com.google.common.collect.Maps;
 import lod.ducksdelights.DucksDelights;
+import net.minecraft.item.Item;
 import net.minecraft.item.equipment.ArmorMaterial;
 import net.minecraft.item.equipment.EquipmentAsset;
 import net.minecraft.item.equipment.EquipmentAssetKeys;
 import net.minecraft.item.equipment.EquipmentType;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 
@@ -15,8 +17,9 @@ import java.util.Map;
 
 public class ModArmorMaterials {
     public static final RegistryKey<EquipmentAsset> HAUNTED_STEEL_MATERIAL_KEY = RegistryKey.of(EquipmentAssetKeys.REGISTRY_KEY, Identifier.of(DucksDelights.MOD_ID, "haunted_steel"));
+    public static final TagKey<Item> REPAIRS_HAUNTED_ARMOR = TagKey.of(RegistryKeys.ITEM, Identifier.of(DucksDelights.MOD_ID, "repairs_haunted_armor"));
     public static final ArmorMaterial HAUNTED_STEEL_MATERIAL = new ArmorMaterial(
-            20, createDefenseMap(2, 5, 7, 2, 6), 20, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 1.0F, 0.0F, ItemTags.REPAIRS_CHAIN_ARMOR, HAUNTED_STEEL_MATERIAL_KEY
+            20, createDefenseMap(2, 5, 7, 2, 6), 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 1.0F, 0.0F, REPAIRS_HAUNTED_ARMOR, HAUNTED_STEEL_MATERIAL_KEY
     );
 
 
