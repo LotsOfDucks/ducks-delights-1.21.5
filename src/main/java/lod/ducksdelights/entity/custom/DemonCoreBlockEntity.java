f2626ca6f3f352e368b660364e9308dc9535277c
LotsOfDucks <113879225+LotsOfDucks@users.noreply.github.com>
2025-05-05 16:46:37 -0700

    Assist

--- src/main/java/lod/ducksdelights/entity/custom/DemonCoreBlockEntity.java
+++ src/main/java/lod/ducksdelights/entity/custom/DemonCoreBlockEntity.java
@@ -4,6 +4,7 @@ import lod.ducksdelights.block.custom.DemonCoreBlock;
 import lod.ducksdelights.entity.ModBlockEntityTypes;
 import lod.ducksdelights.entity.damage.ModDamageTypes;
 import lod.ducksdelights.sound.ModSounds;
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
@@ -26,7 +27,7 @@ public class DemonCoreBlockEntity extends BlockEntity {
     private boolean powered;
     private boolean waterlogged;
     public int ticks;
-    public int range = 20;
+    public int range = 24;
 
     public DemonCoreBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntityTypes.DEMON_CORE, pos, state);
@@ -80,67 +81,13 @@ public class DemonCoreBlockEntity extends BlockEntity {
         }
     }
 
-    public static float calculateReceivedDamage(World world, Vec3d pos1, Vec3d pos2, Vec3d pos3, Vec3d pos4, Vec3d pos5, Vec3d pos6, LivingEntity livingEntity, BlockPos pos, Integer val) {
-        long a = (long) (Math.clamp(Math.ceil(Math.abs(pos.toCenterPos().distanceTo(livingEntity.getPos()))), 1, val));
-        long s = world.getTime();
-        if (s % a == 0L) {
-            Box box = livingEntity.getBoundingBox();
-            double d = 1.0 / ((box.maxX - box.minX) * 2.0 + 1.0);
-            double e = 1.0 / ((box.maxY - box.minY) * 2.0 + 1.0);
-            double f = 1.0 / ((box.maxZ - box.minZ) * 2.0 + 1.0);
-            double g = (1.0 - Math.floor(1.0 / d) * d) / 2.0;
-            double h = (1.0 - Math.floor(1.0 / f) * f) / 2.0;
-            if (!(d < 0.0) && !(e < 0.0) && !(f < 0.0)) {
-                int i = 0;
-                int j = 0;
-
-                for (double k = 0.0; k <= 1.0; k += d) {
-                    for (double l = 0.0; l <= 1.0; l += e) {
-                        for (double m = 0.0; m <= 1.0; m += f) {
-                            double n = MathHelper.lerp(k, box.minX, box.maxX);
-                            double o = MathHelper.lerp(l, box.minY, box.maxY);
-                            double p = MathHelper.lerp(m, box.minZ, box.maxZ);
-                            Vec3d vec3d = new Vec3d(n + g, o, p + h);
-                            Vec3d vec3d2 = new Vec3d(n + g, o + (0.9 * (box.maxY - box.minY)), p + h);
-                            boolean bl = livingEntity.getWorld().raycast(new RaycastContext(pos1, vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl1 = livingEntity.getWorld().raycast(new RaycastContext(pos2, vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl2 = livingEntity.getWorld().raycast(new RaycastContext(pos3, vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl3 = livingEntity.getWorld().raycast(new RaycastContext(pos4, vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl4 = livingEntity.getWorld().raycast(new RaycastContext(pos5, vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl5 = livingEntity.getWorld().raycast(new RaycastContext(pos6, vec3d, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl6 = livingEntity.getWorld().raycast(new RaycastContext(pos1, vec3d2, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl7 = livingEntity.getWorld().raycast(new RaycastContext(pos2, vec3d2, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl8 = livingEntity.getWorld().raycast(new RaycastContext(pos3, vec3d2, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl9 = livingEntity.getWorld().raycast(new RaycastContext(pos4, vec3d2, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl10 = livingEntity.getWorld().raycast(new RaycastContext(pos5, vec3d2, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            boolean bl11 = livingEntity.getWorld().raycast(new RaycastContext(pos6, vec3d2, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
-                            if (!bl || !bl1 || !bl2 || !bl3 || !bl4 || !bl5 || !bl6 || !bl7 || !bl8 || !bl9 || !bl10 || !bl11) {
-                                ++i;
-                            }
-                            ++j;
-                        }
-                    }
-                }
-                return (float) i / (float) j;
-            } else {
-                return 0.0F;
-            }
-        }
-        return 0.0F;
-    }
-
     private static void irradiateEntities(World world, List<LivingEntity> list, BlockPos pos, Integer val) {
         for (LivingEntity livingEntity : list) {
-            if (pos.toCenterPos().isInRange(livingEntity.getPos(), val) && !livingEntity.isInCreativeMode() && livingEntity.getHealth() > 0) {
-                Vec3d vec3d = new Vec3d(pos.toCenterPos().add(0.51).getX(), pos.toCenterPos().getY(), pos.toCenterPos().getZ());
-                Vec3d vec3d1 = new Vec3d(pos.toCenterPos().add(-0.51).getX(), pos.toCenterPos().getY(), pos.toCenterPos().getZ());
-                Vec3d vec3d2 = new Vec3d(pos.toCenterPos().getX(), pos.toCenterPos().add(0.51).getY(), pos.toCenterPos().getZ());
-                Vec3d vec3d3 = new Vec3d(pos.toCenterPos().getX(), pos.toCenterPos().add(-0.51).getY(), pos.toCenterPos().getZ());
-                Vec3d vec3d4 = new Vec3d(pos.toCenterPos().getX(), pos.toCenterPos().getY(), pos.toCenterPos().add(0.51).getZ());
-                Vec3d vec3d5 = new Vec3d(pos.toCenterPos().getX(), pos.toCenterPos().getY(), pos.toCenterPos().add(-0.51).getZ());
-                float q = calculateReceivedDamage(world, vec3d, vec3d1, vec3d2, vec3d3, vec3d4, vec3d5, livingEntity, pos, val);
+            if (pos.toCenterPos().isInRange(livingEntity.getPos(), val) && !livingEntity.isInCreativeMode() && livingEntity.isAlive()) {
+                getRaycastSides(livingEntity, pos);
+                float q = calculateReceivedDamage(world, livingEntity, pos, val);
                 if (q == 1) {
-                    if (livingEntity.isPlayer() && livingEntity.age <= 200 && !livingEntity.isInCreativeMode()) {
+                    if (livingEntity.isPlayer() && livingEntity.age <= 200) {
                         return;
                     } else {
                         applyDamage(world, pos, livingEntity);
@@ -150,16 +97,106 @@ public class DemonCoreBlockEntity extends BlockEntity {
         }
     }
 
-    public static void applyDamage(World world, BlockPos pos, LivingEntity livingEntity) {
-        long a = (long) (Math.clamp(Math.ceil(Math.abs(pos.toCenterPos().distanceTo(livingEntity.getPos()))), 1, 20));
+    private static void getRaycastSides(LivingEntity livingEntity, BlockPos blockPos) {
+        Position entityPositionFeet = livingEntity.getPos();
+        Position entityPositionEyes = livingEntity.getEyePos();
+        double blockCenterPosX = blockPos.toCenterPos().getX();
+        double blockCenterPosY = blockPos.toCenterPos().getY();
+        double blockCenterPosZ = blockPos.toCenterPos().getZ();
+        if (entityPositionEyes.getX() > blockCenterPosX) {
+            Vec3d eyeVectorX = new Vec3d(blockPos.toCenterPos().add(0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+        } else {
+            Vec3d eyeVectorX = new Vec3d(blockPos.toCenterPos().add(-0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+        }
+        if (entityPositionEyes.getY() > blockCenterPosY) {
+            Vec3d eyeVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(0.51).getY(), blockPos.toCenterPos().getZ());
+        } else {
+            Vec3d eyeVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(-0.51).getY(), blockPos.toCenterPos().getZ());
+        }
+        if (entityPositionEyes.getZ() > blockCenterPosZ) {
+            Vec3d eyeVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(0.51).getZ());
+        } else {
+            Vec3d eyeVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(-0.51).getZ());
+        }
+        if (entityPositionFeet.getX() > blockCenterPosX) {
+            Vec3d footVectorX = new Vec3d(blockPos.toCenterPos().add(0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+        } else {
+            Vec3d footVectorX = new Vec3d(blockPos.toCenterPos().add(-0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+        }
+        if (entityPositionFeet.getY() > blockCenterPosY) {
+            Vec3d footVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(0.51).getY(), blockPos.toCenterPos().getZ());
+        } else {
+            Vec3d footVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(-0.51).getY(), blockPos.toCenterPos().getZ());
+        }
+        if (entityPositionFeet.getZ() > blockCenterPosZ) {
+            Vec3d footVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(0.51).getZ());
+        } else {
+            Vec3d footVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(-0.51).getZ());
+        }
+    }
+
+    public static float calculateReceivedDamage(World world, LivingEntity livingEntity, BlockPos blockPos, Integer val) {
+        long a = (long) (Math.clamp(Math.floor(Math.abs(blockPos.toCenterPos().distanceTo(livingEntity.getPos().add(-1)))), 1, val));
         long s = world.getTime();
         if (s % a == 0L) {
-            DamageSource damageSource = new DamageSource(
-                    world.getRegistryManager()
-                            .getOrThrow(RegistryKeys.DAMAGE_TYPE)
-                            .getEntry(ModDamageTypes.FISSION.getValue()).get()
-            );
-            livingEntity.damage((ServerWorld) world, damageSource, 1);
+            Position entityPositionFeet = livingEntity.getPos();
+            Position entityPositionEyes = livingEntity.getEyePos();
+            double blockCenterPosX = blockPos.toCenterPos().getX();
+            double blockCenterPosY = blockPos.toCenterPos().getY();
+            double blockCenterPosZ = blockPos.toCenterPos().getZ();
+            if (entityPositionEyes.getX() > blockCenterPosX) {
+                Vec3d eyeVectorX = new Vec3d(blockPos.toCenterPos().add(0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+            } else {
+                Vec3d eyeVectorX = new Vec3d(blockPos.toCenterPos().add(-0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+            }
+            if (entityPositionEyes.getY() > blockCenterPosY) {
+                Vec3d eyeVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(0.51).getY(), blockPos.toCenterPos().getZ());
+            } else {
+                Vec3d eyeVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(-0.51).getY(), blockPos.toCenterPos().getZ());
+            }
+            if (entityPositionEyes.getZ() > blockCenterPosZ) {
+                Vec3d eyeVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(0.51).getZ());
+            } else {
+                Vec3d eyeVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(-0.51).getZ());
+            }
+            if (entityPositionFeet.getX() > blockCenterPosX) {
+                Vec3d footVectorX = new Vec3d(blockPos.toCenterPos().add(0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+            } else {
+                Vec3d footVectorX = new Vec3d(blockPos.toCenterPos().add(-0.51).getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().getZ());
+            }
+            if (entityPositionFeet.getY() > blockCenterPosY) {
+                Vec3d footVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(0.51).getY(), blockPos.toCenterPos().getZ());
+            } else {
+                Vec3d footVectorY = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().add(-0.51).getY(), blockPos.toCenterPos().getZ());
+            }
+            if (entityPositionFeet.getZ() > blockCenterPosZ) {
+                Vec3d footVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(0.51).getZ());
+            } else {
+                Vec3d footVectorZ = new Vec3d(blockPos.toCenterPos().getX(), blockPos.toCenterPos().getY(), blockPos.toCenterPos().add(-0.51).getZ());
+            }
+            int i = 0;
+            int j = 0;
+            boolean eyesX = livingEntity.getWorld().raycast(new RaycastContext(eyeVectorX, livingEntity.getEyePos(), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
+            boolean eyesY = livingEntity.getWorld().raycast(new RaycastContext(eyeVectorY, livingEntity.getEyePos(), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
+            boolean eyesZ = livingEntity.getWorld().raycast(new RaycastContext(eyeVectorZ, livingEntity.getEyePos(), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
+            boolean footX = livingEntity.getWorld().raycast(new RaycastContext(footVectorX, livingEntity.getEyePos(), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
+            boolean footY = livingEntity.getWorld().raycast(new RaycastContext(footVectorY, livingEntity.getEyePos(), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
+            boolean footZ = livingEntity.getWorld().raycast(new RaycastContext(footVectorZ, livingEntity.getEyePos(), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, livingEntity)).getType() == HitResult.Type.BLOCK;
+            if (!eyesX || !eyesY || !eyesZ || !footX || !footY || !footZ) {
+                ++i;
+            }
+            ++j;
+            return (float) i / (float) j;
         }
+        return 0.0F;
+    }
+
+    public static void applyDamage(World world, BlockPos pos, LivingEntity livingEntity) {
+        DamageSource damageSource = new DamageSource(
+                world.getRegistryManager()
+                        .getOrThrow(RegistryKeys.DAMAGE_TYPE)
+                        .getEntry(ModDamageTypes.FISSION.getValue()).get()
+        );
+        livingEntity.damage((ServerWorld) world, damageSource, 1);
     }
 }
